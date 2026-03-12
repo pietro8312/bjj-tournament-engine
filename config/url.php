f792051b1196ab17b7758781bd474daf88536ae9
pietrodonicht8@gmail.com <pietrodonicht8@gmail.com>
2026-03-11 21:18:13 -0300

    feat: added url configuration
    for better understandment and usability

---
+++ config/url.php
@@ -0,0 +1,12 @@
+<?php 
+    define('BASE_URL', '/proj-irene');
+
+    define('ASSETS_URL', BASE_URL . '/assets');
+
+    define('CSS_URL', ASSETS_URL . '/css/');
+    define('JS_URL', ASSETS_URL . '/js/');
+    define('IMG_URL', ASSETS_URL . '/img/');
+
+    define('CONTROLLERS_URL', BASE_URL . '/controllers/');
+    define('VIEWS_URL', BASE_URL . '/views/');
+?>
